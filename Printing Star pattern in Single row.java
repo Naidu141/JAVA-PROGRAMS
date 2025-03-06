6b9fc95dccdc826d2480f7f7b16a18c42ebdabca
Naidu141 <jogajneelakanta@gmail.com>
2025-03-06 11:30:08 +0530

    Create  Printing Star pattern in Single row.java

---
+++ Printing Star pattern in Single row.java
@@ -0,0 +1,14 @@
+// Printing Star pattern in Single row //
+* * * * *
+public class patternA
+{
+    public static void main(String[] args)
+    {
+        int i;
+        for(i=0;i<5;i++)
+        {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+}
