ef9da34a886fb20d5e465f7260d99178b3c8cd94
Naidu141 <jogajneelakanta@gmail.com>
2025-03-06 11:28:35 +0530

    Create Conditional statements .java

---
+++ Conditional statements .java
@@ -0,0 +1,17 @@
+// Conditional statements programe //
+
+public class Conditions
+{
+    public static void main(String[] args)
+    {
+        int age =20;
+        if(age<=18)
+        {
+            System.out.println("Not Allowed");
+        }
+        else
+        {
+            System.out.println(" Allowed");
+        }
+    }
+}
