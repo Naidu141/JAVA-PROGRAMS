a3fc8dd60297363f8edf5661d490ba5886b2e8be
Naidu141 <jogajneelakanta@gmail.com>
2025-03-06 11:34:12 +0530

    Create Z latter pattern programe.java

---
+++ Z latter pattern programe.java
@@ -0,0 +1,31 @@
+// Z latter pattern programe//
+* * * * * * 
+        *
+      *
+    *
+  *
+* * * * * *
+    
+public class patternz
+{
+    public static void main(String[] args)
+    {
+        int n=5,i,j;
+        for(i=0;i<=n;i++)
+        {
+            for(j=0;j<=5;j++)
+            {
+                if(i==0||i==n||i+j==n)
+                {
+                   System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+}
