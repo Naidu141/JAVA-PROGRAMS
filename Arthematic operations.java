1f37952afc4f71828ed34eba3250599dc10e5253
Naidu141 <jogajneelakanta@gmail.com>
2025-03-06 11:25:06 +0530

    Create Arthematic operations.java

---
+++ Arthematic operations.java
@@ -0,0 +1,19 @@
+// Arthematic operations programe //
+
+public class Amoperations
+{
+    public static void main(String[] args)
+    {
+        int a=5;
+        int b=4;
+        int add=a+b;
+        int sub=a-b;
+        int mul=a*b;
+        int div=a/b;
+        int mod=a%b;
+        System.out.println("Add:"+add);
+        System.out.println("Sub:"+sub);
+        System.out.println("Mul:"+mul);
+        System.out.println("Div:"+div);
+        System.out.println("Mod:"+mod);
+    }
