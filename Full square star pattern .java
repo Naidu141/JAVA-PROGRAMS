b23fa5e5949585a762aa8822acf7c71f9b7b8d2f
Naidu141 <jogajneelakanta@gmail.com>
2025-03-06 11:29:07 +0530

    Create  Full square star pattern .java

---
+++ Full square star pattern .java
@@ -0,0 +1,23 @@
+// Full square star pattern program //
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *   
+    
+public class fullpattern
+{
+    public static void main(String[] args)
+    {
+        int n=5,i,j;
+        for(i=0;i<=n;i++)
+        {
+            for(j=0;j<=n;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
