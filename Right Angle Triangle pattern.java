d2c5e1839533d541b6a2c25efd6e6619fe8773c0
Naidu141 <jogajneelakanta@gmail.com>
2025-03-06 11:35:27 +0530

    Create Right Angle Triangle pattern.java

---
+++ Right Angle Triangle pattern.java
@@ -0,0 +1,24 @@
+// Right Angle Triangle pattern //
+
+*
+* *
+* * *
+* * * *
+* * * * * 
+    
+public class triangler
+{
+    public static void main(String[] args)
+    {
+        int n=5,i,j;
+        for(i=0;i<=n;i++)
+        {
+            for(j=0;j<=i;j++)
+            {
+                   System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
