820203bccadc9c64cd88cb5b3d455174b2fdf82f
Naidu141 <jogajneelakanta@gmail.com>
2025-03-06 11:33:40 +0530

    Create C letter pattern programe.java

---
+++ C letter pattern programe.java
@@ -0,0 +1,35 @@
+/ C letter pattern programe //
+  * * * * * 
+*
+*
+*
+*
+  * * * * *
+      
+public class patternc
+{
+    public static void main(String[] args)
+    {
+        int n=5,i,j;
+        for(i=0;i<=n;i++)
+        {
+            for(j=0;j<=5;j++)
+            {
+                if(i==0||i==n||j==0)
+                {
+                    if((i==0 && j==0)||(i==5 && j==0))
+                    {
+                        System.out.print("  ");
+                    }
+                   System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+}
