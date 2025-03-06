bf6220026aab476a53d73e624be910c08269cf15
Naidu141 <jogajneelakanta@gmail.com>
2025-03-06 11:27:24 +0530

    Create Printing Star Pattern .java

---
+++ Printing Star Pattern .java
@@ -0,0 +1,19 @@
+// Printing Star Pattern in Single Column //
+*
+*
+*
+*
+*
+    
+public class cline
+{
+    public static void main(String[] args)
+    {
+        int i;
+        for(i=0;i<=5;i++)
+        {
+            System.out.println("* ");
+        }
+        
+    }
+}
