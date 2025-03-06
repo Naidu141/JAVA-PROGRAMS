7f2b5958440ce56f1992f27cc7aa357701c97972
Naidu141 <jogajneelakanta@gmail.com>
2025-03-06 11:43:58 +0530

    Update Variables programe.java

--- Variables programe.java
+++ Variables programe.java
@@ -5,7 +5,7 @@ public class Variables
     public static void main(String[] args)
     {
         int id=5;
-        String name="Suneel";
+        String name="Sai";
         double marks = 19.55;
         System.out.println("id"+" "  +id +" " +"Name"+" "  +name+" " +"Marks"+" " +marks);
         
